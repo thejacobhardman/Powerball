4a82a9645876371d15995b6a2bb46a9e5fcfebee
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-03-06 22:17:47 -0700

    Update Powerball.py
    Finished the main program.
    
    Fixed the winnings calculation bug and finished looping the program so that the user can play for as long as they would like.

--- Powerball.py
+++ Powerball.py
@@ -20,6 +20,9 @@ Is_Running = True
 # Tracks the user's input
 User_Input = ""
 
+# Tracks if the user has made a decision
+User_Confirm = False
+
 # Checks if the user has guessed a valid number
 Valid_Guess = False
 
@@ -40,6 +43,12 @@ Winning_Powerball = 1
 
 # Stores the number of correct numbers
 Correct_Numbers = 0
+
+# Tracks if the user guessed the powerball correctly
+Correct_PowerBall = False
+
+# Stores how much the user has won
+Winnings = ["$0.00", "$1.00", "$2.00", "$10.00", "$20.00", "$100.00", "$1,000.00"]
 
 ########################################################### PROGRAM LOGIC ################################################################
 
@@ -89,7 +98,7 @@ def Manual_Guess():
     while Valid_Guess == False:
         User_Input = input("Please enter your first guess: ")
         if int(User_Input) >= 1 and int(User_Input) <= 9:
-            User_Ticket.append(User_Input)
+            User_Ticket.append(int(User_Input))
             break
         else:
             print("Please enter a valid guess.\n")
@@ -98,7 +107,7 @@ def Manual_Guess():
     while Valid_Guess == False:
         User_Input = input("Please enter your second guess: ")
         if User_Input not in User_Ticket and int(User_Input) >= 1 and int(User_Input) <= 9:
-            User_Ticket.append(User_Input)
+            User_Ticket.append(int(User_Input))
             break
         else:
             print("Please enter a valid guess.\n")
@@ -107,7 +116,7 @@ def Manual_Guess():
     while Valid_Guess == False:
         User_Input = input("Please enter your third guess: ")
         if User_Input not in User_Ticket and int(User_Input) >= 1 and int(User_Input) <= 9:
-            User_Ticket.append(User_Input)
+            User_Ticket.append(int(User_Input))
             break
         else:
             print("Please enter a valid guess.\n")
@@ -150,7 +159,7 @@ def Random_Guess():
             continue
 
 ### Checks the player's guess (either manual or random) against the winning ticket
-def Check_Result():
+def Display_Result():
 
     # Importing global variables
     global User_Ticket
@@ -158,16 +167,15 @@ def Check_Result():
     global Winning_Ticket
     global Winning_Powerball
 
-    print("Your Ticket: ", end="")
+    print("\nYour Ticket: ", end="")
     for Number in User_Ticket:
         print(Number, end=" ")
-    print(User_Powerball)
-    input("Press 'enter to continue.")
+    print("| " + str(User_Powerball))
 
     print("The Drawing: ", end="")
     for Number in Winning_Ticket:
         print(Number, end=" ")
-    print(Winning_Powerball)
+    print("| " + str(Winning_Powerball))
     input("Press 'enter' to continue.")
 
 ### Calculates how much the user won based on the number of correct numbers
@@ -179,11 +187,87 @@ def Calculate_Winnings():
     global Winning_Ticket
     global User_Powerball
     global Correct_Numbers
+    global Correct_PowerBall
+    global Winnings
 
+    for Number in User_Ticket:
+       if Number in Winning_Ticket:
+           Correct_Numbers += 1
+       else:
+           continue
+
+    if User_Powerball == Winning_Powerball:
+        Correct_PowerBall = True
+    else:
+        Correct_PowerBall = False
+
+    if Correct_Numbers == 1 and Correct_PowerBall == False:
+        print("\nCongratulations!")
+        print("You matched 1 number but not the PowerBall.")
+        print("You won: " + Winnings[1])
+    elif Correct_Numbers == 1 and Correct_PowerBall == True:
+        print("\nCongratulations!")
+        print("You matched 1 number and the PowerBall.")
+        print("You won: " + Winnings[2])
+    elif Correct_Numbers == 2 and Correct_PowerBall == False:
+        print("\nCongratulations!")
+        print("You matched 2 numbers but not the PowerBall.")
+        print("You won: " + Winnings[3])
+    elif Correct_Numbers == 2 and Correct_PowerBall == True:
+        print("\nCongratulations!")
+        print("You matched 2 number and the PowerBall.")
+        print("You won: " + Winnings[4])
+    elif Correct_Numbers == 3 and Correct_PowerBall == False:
+        print("\nCongratulations!")
+        print("You matched 3 number but not the PowerBall.")
+        print("You won: " + Winnings[5])
+    elif Correct_Numbers == 3 and Correct_PowerBall == True:
+        print("\nCongratulations!")
+        print("You matched 3 numbers and the PowerBall.")
+        print("You won: " + Winnings[6])
+    else:
+        print("\nBetter luck next time!")
+        print("You didn't match any ticket numbers.")
+        print("You won: " + Winnings[0])
+
+### Asking the player if they want to play again and reseting the game
+def Reset_Game():
+
+    # Importing global variables
+    global Is_Running
+    global User_Confirm
+    global User_Ticket
+    global User_Powerball
+    global Winning_Ticket
+    global Winning_Powerball
+    global Correct_Numbers
+    global Correct_PowerBall
+
+    User_Ticket = []
+    User_Powerball = 0
+    Winning_Ticket = []
+    Winning_Powerball = 1
+    Correct_Numbers = 0
+    Correct_PowerBall = False
+
+    while User_Confirm == False:
+        User_Input = input("\nWould you like to play again? (y/n): ")
+        if User_Input.upper() == "Y":
+            break
+        elif User_Input.upper() == "N":
+            Is_Running = False
+            break
+        else:
+            print("\nPlease enter a valid selection.")
+            input("Press 'enter' to continue.")
 
 ########################################################### PROGRAM FLOW #################################################################
 
 while Is_Running == True:
+
+    # Clearing the screen for replayability
+    cls()
+
     print("Welcome to PowerBall!!!".center(100, " "))
 
     print("\nPlease enter '1' to select your numbers manually.")
@@ -193,12 +277,15 @@ while Is_Running == True:
     if User_Input == "1":
         Draw_Ticket() # Generates the winning ticket and PowerBall
         Manual_Guess() # The player makes their guesses
-        Check_Result() # Checks the player's guess against the winning ticket
+        Display_Result() # Checks the player's guess against the winning ticket
+        Calculate_Winnings() # Calculates the amount that the player won
+        Reset_Game() # Resets the game for another playthrough
     elif User_Input == "2":
         Draw_Ticket() # Generates the winning ticket and PowerBall
         Random_Guess() # The computer generates the player's random guess
-        Check_Result() # Checks the player's guess against the winning ticket
+        Display_Result() # Checks the player's guess against the winning ticket
+        Calculate_Winnings() # Calculates the amount that the player won
+        Reset_Game() # Resets the game for another playthrough
     else:
         print("\nPlease enter a valid selection.")
         input("\nPress 'enter' to continue.")
-        cls()
